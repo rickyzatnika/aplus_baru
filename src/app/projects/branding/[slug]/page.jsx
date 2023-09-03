1c892f17adfdca8ead25bb1171ad7600dd27bf7c
aplusMultiKreasi <sayhi@aplusdesign@gmail.com>
2023-09-03 22:12:16 +0700

    change API URL

--- src/app/projects/branding/[slug]/page.jsx
+++ src/app/projects/branding/[slug]/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 async function getData(slug) {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_DEV}/api/branding/${slug}`,
+        `${process.env.NEXT_PUBLIC_API}/api/branding/${slug}`,
         {
             cache: "no-store", // this will fresh data on every fetch request;
             // next: { revalidate: 10 }, // and this , will be refresh data every 10 seconds;
