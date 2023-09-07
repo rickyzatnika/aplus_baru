6ce30ba2f567a47048938e8840cc0abefcb1ca0e
aplusMultiKreasi <sayhi@aplusdesign@gmail.com>
2023-09-07 22:50:08 +0700

    change api url contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -26,7 +26,7 @@ const ContactForm = ({ openForm, setOpenForm }) => {
     }
     try {
       setLoading(true);
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/contact`, {
+      const res = await fetch(`/api/contact`, {
         headers: {
           "Content-Type": "application/json",
         },
