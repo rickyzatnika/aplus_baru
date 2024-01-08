c80e3302e28046f005d483f893cf9fa487e6fdde
aplusMultiKreasi <sayhi@aplusdesign@gmail.com>
2024-01-09 04:12:23 +0700

    change text color animation

--- src/app/projects/branding/page.jsx
+++ src/app/projects/branding/page.jsx
@@ -44,7 +44,7 @@ const Branding = () => {
         <div className="h-full w-24 md:w-96 absolute top-0 right-0 bg-gradient-to-l from-black to-transparent z-10"></div>
         <div className="target block  relative text-5xl lg:text-7xl  font-bold text-center  py-6 w-full  ">
           <span className="hidden">Branding</span>
-          <div className="absolute z-5 top-[50%] right-0 -translate-y-[50%] m-0 whitespace-nowrap opacity-100 transition-opacity text-white ">
+          <div className="absolute z-5 top-[50%] right-0 -translate-y-[50%] m-0 whitespace-nowrap opacity-100 transition-opacity text-transparent bg-clip-text bg-gradient-to-tr from-black to-red-600/90  ">
             <span className="ml-10 md:ml-14 uppercase ">
               Branding
             </span>
