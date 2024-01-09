ca4a9e2261086f3075a50bb42f3f1d5865090ba0
aplusMultiKreasi <sayhi@aplusdesign@gmail.com>
2024-01-09 11:06:04 +0700

    fix mobile responsive

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -12,8 +12,7 @@ const NavMobile = ({ open, setOpen, openForm, setOpenForm }) => {
 
   return (
     <>
-      <nav className={`fixed w-full min-h-screen bg-[#080808] top-0 left-0 flex flex-col items-start justify-between gap-5 lg:hidden transition-all duration-300 ease-in-out ${open ? " left-0" : "left-[-100%]"} `}>
-      
+      <nav className={`fixed w-full h-screen bg-[#080808] top-0 left-0 flex flex-col items-start justify-between  lg:hidden transition-all duration-300 ease-in-out ${open ? " left-0" : "left-[-100%]"} `}>
         <div></div>
         <ul className='gap-5 flex flex-col items-start text-lg pl-6 text-zinc-300'>
           <li className='flex items-center gap-2'>
@@ -46,24 +45,23 @@ const NavMobile = ({ open, setOpen, openForm, setOpenForm }) => {
           </li>
         </ul >
        
-        <div className='w-full flex items-center gap-3 py-3 px-2 flex-col justify-center bg-gradient-to-tr from-red-700 to-orange-600'>
-          <span className='text-zinc-200'>Follow Us :</span>
-          <div className='w-full flex gap-4 items-center justify-center  text-zinc-200 '>
+        <div className='w-full flex items-center gap-2 py-2 px-1 flex-col justify-center bg-gradient-to-tr from-red-700 to-orange-600'>
+          <span className='text-zinc-200 text-xs tracking-widest'>Follow Us :</span>
+          <div className='w-full flex gap-4 pb-2 items-center justify-center  text-zinc-200 '>
             <Link passHref={true} href="">
-              <ImFacebook2 size={20} />
+              <ImFacebook2 size={22} />
             </Link>
             <Link passHref={true} href="https://api.whatsapp.com/send?phone=6281214707415&text=Hallo%20Aplus" target='_blink'>
-              <FaWhatsapp size={20} />
+              <FaWhatsapp size={22} />
             </Link>
             <Link passHref={true} href="https://www.instagram.com/aplusmultikreasi.id" target='_blink'>
-              <GrInstagram size={20} />
+              <GrInstagram size={22} />
             </Link>
             <Link passHref={true} href="https://www.youtube.com/@hello_aplus7424" target='_blink'>
-              <FaYoutube size={20} />
+              <FaYoutube size={22} />
             </Link>
           </div>
         </div>
-
       </nav>
     </>
   )
