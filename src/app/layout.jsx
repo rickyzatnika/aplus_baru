1bd63c9b5ea097d699d90b8c7c0d49f77177be09
rickyzatnika <rickyzatnika91@gmail.com>
2024-10-26 23:09:24 +0700

    google analitycs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children, session }) {
 					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-MVFKQBS7');
+					})(window,document,'script','dataLayer', ${GTM_ID},);
           `}
       </Script>
 
@@ -40,7 +40,7 @@ export default function RootLayout({ children, session }) {
 
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVFKQBS7"height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
       </body>
