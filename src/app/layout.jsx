94b88fc4514d715c95ad6ff04579ffdc6398b80e
rickyzatnika <rickyzatnika91@gmail.com>
2024-10-26 22:54:08 +0700

    add google tag manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Navbar from "@/components/Navbar";
 import "./globals.css";
@@ -10,17 +10,11 @@ import Footer from "@/components/Footer";
 import { Poppins } from "next/font/google";
 import AOSInit from "@/components/AOSInit";
 
+const poppins = Poppins({ subsets: ["latin"], weight: "400", display: "swap" });
 
-
-const poppins = Poppins({ subsets: ["latin"], weight: "400", display: "swap", });
-
-
-const GTM_ID = "G-LJ0KYVQ00T"
+const GTM_ID = "G-LJ0KYVQ00T";
 
 export default function RootLayout({ children, session }) {
-
-
-
   return (
     <html lang="en">
       {/* Google tag (gtag.js)  */}
@@ -28,18 +22,17 @@ export default function RootLayout({ children, session }) {
       <Script id="google-tag-manager" strategy="afterInteractive">
         {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-MVFKQBS7');
           `}
       </Script>
 
       <body className={`${poppins.className} `}>
-
         <AOSInit />
         <ToastContainer position="top-center" />
-        <SessionProvider session={session} >
+        <SessionProvider session={session}>
           <Navbar />
           {children}
           <Footer />
@@ -47,7 +40,7 @@ export default function RootLayout({ children, session }) {
 
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVFKQBS7"height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
       </body>
