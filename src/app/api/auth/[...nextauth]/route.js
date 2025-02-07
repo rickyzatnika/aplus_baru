9ba64c843ce78037efa0fadcc84e341f6bdcf021
rickyzatnika <rickyzatnika91@gmail.com>
2025-02-07 22:58:06 +0700

    error handling authentication

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,65 +1,9 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 import connect from "@/utils/db";
-import User from "@/models/User";
+import Users from "@/models/User";
 import { compare } from "bcryptjs";
-
-// async function login(credentials) {
-//   try {
-//     await connect();
-//     const user = await User.findOne({ username: credentials.username });
-//     if (!user) throw new Error("Wrong Credentials.");
-//     const isCorrect = await compare(credentials.password, user.password);
-//     if (!isCorrect) throw new Error("Wrong Credentials.");
-//     return user;
-//   } catch (error) {
-//     console.log("error while logging in.");
-//     throw new Error("something went wrong!");
-//   }
-// }
-
-// export const authOptions = {
-//   providers: [
-//     CredentialsProvider({
-//       name: "credentials",
-//       credentials: {},
-//       async authorize(credentials) {
-//         try {
-//           const user = await login(credentials);
-//           return user;
-//         } catch (error) {
-//           throw new Error("Failed to login.");
-//         }
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     async jwt({ token, user }) {
-//       if (user) {
-//         token.username = user.username;
-//         token.email = user.email;
-//         token.isAdmin = user.isAdmin;
-//         token.id = user.id;
-//       }
-//       return token;
-//     },
-//     async session({ session, token }) {
-//       if (token) {
-//         session.user.username = token.username;
-//         session.user.email = token.email;
-//         session.user.isAdmin = token.isAdmin;
-//         session.user.id = token.id;
-//       }
-//       return session;
-//     },
-//   },
-//   pages: {
-//     signIn: "/",
-//     signOut: "/",
-//   },
-// };
-
-// const handler = NextAuth(authOptions);
+import { signJwtToken } from "@/utils/jwt";
 
 const handler = NextAuth({
   providers: [
@@ -68,23 +12,23 @@ const handler = NextAuth({
 
       async authorize(credentials, req) {
         await connect();
-
         const { name, password } = credentials;
 
-        const user = await User.findOne({ name });
+        const user = await Users.findOne({ name });
 
         if (!user) {
-          throw new Error("User tidak ditemukan!");
+          throw new Error("User tidak ditemukan, silahkan registrasi!");
         }
         const isPassword = await compare(password, user.password);
 
         if (!isPassword) {
-          throw new Error("Password salah!");
+          throw new Error("Password atau username salah!");
         } else {
           const { password, ...currentUser } = user._doc;
+          const accessToken = signJwtToken(currentUser, { expiresIn: "1d" });
           return {
             ...currentUser,
-            user,
+            accessToken,
           };
         }
       },
@@ -93,22 +37,18 @@ const handler = NextAuth({
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.username = user.username;
-        token.email = user.email;
-        token.isAdmin = user.isAdmin;
-        token.id = user.id;
+        token.accessToken = user.accessToken;
+        token._id = user._id;
+        token.role = user.role;
       }
-      console.log("this is the token = ", token);
       return token;
     },
     async session({ session, token }) {
       if (token) {
-        session.user.username = token.username;
-        session.user.email = token.email;
-        session.user.isAdmin = token.isAdmin;
-        session.user.id = token.id;
+        session.user._id = token._id;
+        session.user.accessToken = token.accessToken;
+        session.user.role = token.role || "user";
       }
-      console.log("this is the session = ", session);
       return session;
     },
   },
